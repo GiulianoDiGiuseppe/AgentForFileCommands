8ad3cded6a534c6f6d67e7bac662547ee97e2d53
Giuliano Di Giuseppe <digigiu00@gmail.com>
2024-11-01 22:58:03 +0100

    formatt test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,24 +1,28 @@
 import pytest
+import os
 from httpx import AsyncClient
 from src.app import app
 
+
 @pytest.mark.asyncio
 async def test_post_agent_with_valid_payload():
-    payload = {
-        "msg": "I want to find all files in directory 'C:/Users/digig/Desktop/Works/AgentForFileCommands/src'"
-    }
-    
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    msg = f"I want to find all files in directory '{parent_directory}/src'"
+    payload = {"msg": msg}
+
     async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.post("/agent", json=payload)
-    
+
     assert response.status_code == 200
     response_data = response.json()
     assert "msg" in response_data  # Modifica in base alla tua risposta attesa
+
 
 @pytest.mark.asyncio
 async def test_post_agent_invalid_payload():
     payload = {"invalid_key": "value"}
     async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.post("/agent", json=payload)
-    
+
     assert response.status_code == 422  # Unprocessable Entity
